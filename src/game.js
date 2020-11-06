0829aeae36bbcda2c3fb17cdcec972a9fae31ea8
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-11-06 12:35:07 -0700

    Added winning conditions nested array

--- src/game.js
+++ src/game.js
@@ -1,10 +1,32 @@
 class Game {
   constructor(playerOne, playerTwo){
     this.players = [playerOne, playerTwo];
+    this.turnCount = 0;
+    this.winningPlay = [];
+    this.winConditions =
+      [{
+        horizontal: [
+            {option: [1,2,3]},
+            {option: [4,5,6]},
+            {option: [7,8,9]}
+            ],
+        vertical: [
+            {option: [1,4,7]},
+            {option: [2,5,8]},
+            {option: [3,6,9]}
+            ],
+        diagnol: [
+          {option: [1,5,9]},
+          {option: [3,5,7]},
+          ],
+      }];
+
   }
 }
 
-var playerOne = new Player(1,"X",4);
-var playerTwo = new Player(2,"O",12);
+
+
+var playerOne = new Player(1);
+var playerTwo = new Player(2);
 
 var newGame = new Game(playerOne, playerTwo);
--- src/player.js
+++ src/player.js
@@ -1,28 +1,24 @@
 class Player {
-  constructor(playerId, playerTolken, playerWins){
+  constructor(playerId){
     this.playerId = playerId;
-    this.playerTolken = playerTolken;
-    this.playerWins = playerWins;
+    this.playerTolken = "";
+    this.playerWins = 0;
+
+    if(playerId === 1){
+      this.playerTolken = "&#x1f379"
+    } else {
+      this.playerTolken = "&#129449;"
+    }
   }
   saveWins(playerWins){
-    localStorage.setItem(`"${this.playerId}"`, JSON.stringify(playerWins));
+    localStorage.setItem(`Player${this.playerId} Win Count`, JSON.stringify(this.playerWins));
   }
 
   retrieveWins(){
-    var parsedPlayer = JSON.parse(localStorage.getItem(`"${this.playerId}"`));
+    var parsedPlayer = JSON.parse(localStorage.getItem(`Player${this.playerId} Win Count`));
     for (var i = 0; i < parsedPlayer.length; i++){
       parsedPlayer[i].wins = this.playerWins;
     }
     return this.playerWins;
   }
-
 }
-
-// var playerOne = new Player(1,"X",4);
-// var playerTwo = new Player(2,"X",12);
-//
-// playerOne.saveWins(playerOne);
-// playerTwo.saveWins(playerTwo);
-//
-// playerOne.retrieveWins();
-// playerTwo.retrieveWins();
