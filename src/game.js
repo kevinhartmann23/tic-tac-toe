02f622a126e1ea1e7f28b12b8b47fe0f0af6ae4f
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-11-07 12:57:32 -0700

    Function is returning 21/24 possible condtions, not showing 3 consecutive indices greater than -1

--- src/game.js
+++ src/game.js
@@ -3,8 +3,8 @@ class Game {
     this.turn = playerOne || playerTwo;
     this.turnCount = 0;
     this.possibleMoveValues = [1,2,3,4,5,6,7,8,9]
-    playerOne.moves = [1,2,3,5];
-    playerTwo.moves = [6,8,9];
+    playerOne.moves = [1,2,5];
+    playerTwo.moves = [6,9,3,8];
     this.win = false;
     this.draw = false;
 
@@ -36,42 +36,22 @@ class Game {
     console.log("Turn Count Changed To", this.turnCount);
   }
 
-  recordPlayersTurn(selectedMove){
-    var player = this.turn;
-    for(var i = 0; i < this.possibleMoveValues.length; i++){
-      if(this.possibleMoveValues.includes(selectedMove)){
-        player.moves.push(selectedMove);
-        this.possibleMoveValues.splice(this.possibleMoveValues.indexOf(selectedMove), 1);
-      }
-    }
-    console.log(player.moves);
-    console.log(this.possibleMoveValues);
-  }
-
   checkBoardForWin(player){
-    var winningCombination = [];
     for(var i = 0; i < this.winCombinations.length; i++){
-      debugger;
-      for(var j = 0; j < this.winCombinations[i].length; j++){
-        if(player.moves.includes(this.winCombinations[i][j])){
-          var combinationNumber = this.winCombinations[i][j];
-        }
-        if(!winningCombination.includes(combinationNumber)){
-            winningCombination.push(combinationNumber);
-        }
-        if(this.winCombinations[i] !== winningCombination){
-            this.win;
-        } else {
-          return this.win = true;
-        }
+      if(player.moves.indexOf(this.winCombinations[i][0])) {
+        console.log(player.moves.indexOf(this.winCombinations[i][0]))
+        if(player.moves.indexOf(this.winCombinations[i][1])){
+          console.log(player.moves.indexOf(this.winCombinations[i][1]))
+          if(player.moves.indexOf(this.winCombinations[i][2])){
+            console.log(player.moves.indexOf(this.winCombinations[i][2]))
+            player.wins++;
+            this.win = true;
+            }
           }
-          }
-      console.log(winningCombination);
-      console.log(player);
-      console.log(this.win);
-      //CLOSE! Need to find a way to break loop when winning winningCombination matches winCombinations
-      // I may need tostring? right now my loop is giving me exactly what the player.moves array is.
-   };
+        }
+      };
+    };
+
 
   checkBoardForDraw(){
     if(this.possibleMoveValues.length === 0){
@@ -87,6 +67,8 @@ class Game {
     playerOne.moves = [];
     playerTwo.moves = [];
   };
+
+
 }
 
 
@@ -95,7 +77,6 @@ var playerOne = new Player(1);
 var playerTwo = new Player(2);
 
 var newGame = new Game(playerOne, playerTwo);
-
 
 //Questions for Mentor:
 //1) At first I have a this.players = [playerOne, playerTwo] in the constructor,
