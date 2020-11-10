0de7555d8d9e04c3f874a00740705e5d4067dd9f
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-11-10 13:42:33 -0700

    Matched style guide to player.js

--- src/player.js
+++ src/player.js
@@ -1,28 +1,28 @@
 class Player {
   constructor(playerId){
     this.id = playerId;
-    this.token = "";
+    this.token = '';
     this.wins = 0;
 
-    if(playerId === 1){
+    if (playerId === 1){
       this.token = '🍹'
     } else {
       this.token = '🦩'
-    }
-  }
-
-  saveWins(){
-    localStorage.setItem(`Player${this.id} Win Count`, JSON.stringify(this.wins));
-  }
-
-  retrieveWins(){
-    var parsedPlayer = JSON.parse(localStorage.getItem(`Player${this.id} Win Count`));
-    return this.wins = parsedPlayer;
+    };
   };
 
-  resetWins(){
-    this.wins = 0;
-    localStorage.setItem(`Player${this.id} Win Count`, JSON.stringify(this.wins));
-  }
+saveWins(){
+  localStorage.setItem(`Player${this.id} Win Count`, JSON.stringify(this.wins));
+};
 
-}
+retrieveWins(){
+  var parsedPlayer = JSON.parse(localStorage.getItem(`Player${this.id} Win Count`));
+  return this.wins = parsedPlayer;
+};
+
+resetWins(){
+  this.wins = 0;
+  localStorage.setItem(`Player${this.id} Win Count`, JSON.stringify(this.wins));
+};
+
+};
